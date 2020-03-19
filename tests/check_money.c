4d8d7d4d2769bb01d1bb04b62590318544b783d0
roberthliu <robert@cybeats.com>
2020-03-19 12:23:02 -0400

    Jenkinsfile Tap/JUnit

--- tests/check_money.c
+++ tests/check_money.c
@@ -7,7 +7,7 @@ START_TEST(test_money_create) {
   extern Money *money_create(int, char*);
 
   m = money_create(5, "USD");
-  ck_assert_int_eq(money_amount(m), 5);
+  ck_assert_int_eq(money_amount(m), 6);
   ck_assert_str_eq(money_currency(m), "USD");
   money_free(m); 
 } END_TEST 
