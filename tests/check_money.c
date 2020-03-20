78e49da53b8bccbcef24c9af5a85a49ca8b8b766
roberthliu <robert@cybeats.com>
2020-03-20 14:58:18 -0400

    test

--- tests/check_money.c
+++ tests/check_money.c
@@ -7,7 +7,7 @@ START_TEST(test_money_create) {
   extern Money *money_create(int, char*);
 
   m = money_create(5, "USD");
-  ck_assert_int_eq(money_amount(m), 6);
+  ck_assert_int_eq(money_amount(m), 5);
   ck_assert_str_eq(money_currency(m), "USD");
   money_free(m); 
 } END_TEST 
@@ -17,7 +17,7 @@ Suite *money_suite(void) {
   Suite *s;
   TCase *tc_core;
 
-  s = suite_create("Money");
+  s = suite_create("com.cybeats.fwup.core Money");
   tc_core = tcase_create("Core");
 
   tcase_add_test(tc_core, test_money_create);
@@ -34,7 +34,7 @@ int main(void) {
   s = money_suite();
   runner = srunner_create(s);
 
-  srunner_set_xml(runner, "./check-report.xml");
+  srunner_set_xml(runner, "");
 //  srunner_set_tap(runner, "./tap-report.txt");
   srunner_run_all(runner, CK_NORMAL); 
   no_failed = srunner_ntests_failed(runner);
